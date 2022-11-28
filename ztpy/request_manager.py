d637a346d6083514a1645f5042dfc6485c503c9d
Aldo Forte <98174165+Aldo-Forte@users.noreply.github.com>
2022-11-28 12:09:22 +0100

    Add files via upload

---
+++ ztpy/request_manager.py
@@ -0,0 +1,18 @@
+import requests
+import json
+from ztpy.zerotier import ZeroTier
+
+
+class RequestManager:
+
+    @staticmethod
+    def do_get_request(resource: str) -> dict:
+        header_auth = {'Authorization': 'Bearer ' + ZeroTier.get_token()}
+        result = requests.get(f'https://api.zerotier.com/api/v1/{resource}', headers=header_auth)
+        result_data = result.text
+        data = json.loads(result_data) 
+        return data
+
+    def do_post_request(resource: str, data: dict ):
+        pass
+    
---
+++ ztpy/zerotier.py
@@ -0,0 +1,16 @@
+class ZeroTier:    
+
+    __token = "";
+
+    @staticmethod
+    def set_token(token:str):
+        ZeroTier.__token = token
+
+    @staticmethod
+    def get_token() -> str:
+        return ZeroTier.__token
+
+    
+
+
+    
